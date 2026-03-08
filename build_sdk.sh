643e50e068b14fd5c51ca5aa8ed1c5db47ecaf21
Nikita Ostrenkov <ownik.dev@gmail.com>
2026-03-08 09:47:57 +0300

    build_sdk.sh: remove sha256sum generating of release sdk files
    github already calculate hash sum of release files

--- build_sdk.sh
+++ build_sdk.sh
@@ -14,9 +14,3 @@ make -j$(nproc) tools/install
 make -j$(nproc) toolchain/install
 make -j$(nproc) target/compile
 make -j$(nproc) target/sdk/compile
-
-for file in $PWD/bin/targets/${ARCH}/*/entware-sdk-${ARCH}*; do
-    dir=$(dirname $file)
-    name=$(basename $file)
-    (cd $dir; sha256sum $name) >> $dir/$name.sha256
-done
